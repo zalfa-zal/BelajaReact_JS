df6aae0e9a11eb20feecd888ffa117a5bebfc559
zalfa-zal <zahirahzalfa223@gmail.com>
2026-01-21 10:16:53 +0700

    add component css

--- projectPertama/src/App.jsx
+++ projectPertama/src/App.jsx
@@ -3,33 +3,36 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Greeting(){
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="title">
+      <h1>Halo</h1>
+      <p>Selamat pagi</p>
+    </div>
     </>
   )
+}
+
+//props/properti
+function Biodata(props){
+  return (
+    <div className="biodata-card">
+    <p>Nama : {props.nama}</p>
+    <p>Kelas : {props.kelas}</p>
+    <p>nisn : {props.nisn}</p>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <Greeting/>
+      <Biodata nama="Zalfa" kelas="XI PPLG 1" nisn="00976"/>
+    </>
+    
+  );
 }
 
 export default App
